ca16a86c87ceae236c542ebb799af6f45325afa9
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-22 17:55:54 -0300

    feat(navigation): adiciona link de navegação para página de produtos" -m "Cria uma seção de navegação com uma div dedicada e adiciona um link apontando para a rota de produtos. Implementa estilização em CSS utilizando flexbox e ajustes visuais para aprimorar o layout e a usabilidade dos links e botões da barra de navegação.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,10 @@ export function Home() {
     return (
         <div>
             <h1>Página Home</h1>
+
+            <nav>
+                <a href="/products">Produtos</a>
+            </nav>
         </div>
     )
 }
