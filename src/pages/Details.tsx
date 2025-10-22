10eacc6923b8672754474b32c513ce22de72564c
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-22 19:20:23 -0300

    feat(product): Cria página de detalhes e configura rota com parâmetro ID
    Implementa a nova página de visualização de detalhes de produtos na aplicação.
    
    - **Componente:** Adicionado o arquivo `details.tsx` para a renderização da interface de detalhes.
    - **Roteamento:** Configurado o roteamento principal da aplicação para aceitar um parâmetro de ID na URL (ex: `/produto/:id`).
    
    Isso permite que a aplicação carregue e exiba informações específicas do produto com base no identificador passado na rota.

---
+++ src/pages/Details.tsx
@@ -0,0 +1,5 @@
+export function Details () {
+    return (
+        <h1>Detalhes</h1>
+    )
+}
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,13 +1,16 @@
 import { Routes, Route} from "react-router"
 import { Home } from "../pages/home"
+import { Details } from "../pages/Details"
 import { Products } from "../pages/Products"
 import { NotFound } from "../pages/NotFound"
+
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/" index element={<Home />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/details/:id" element={<Details />} />
 
             <Route path="*" element={<NotFound />} />
         </Routes>
