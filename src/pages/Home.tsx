08f39c4f84b47e35696e5d421823e2338a8c15b3
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-10-22 19:13:26 -0300

    feat: Implementação navegação programática usando `useNavigate`

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,12 @@
+import { useNavigate} from "react-router"
+
 export function Home() {
+    const navigate = useNavigate()
+
+    function productsOpen() {
+        navigate("/products")
+    }
+    
     return (
         <div>
             <h1>Página Home</h1>
@@ -6,6 +14,8 @@ export function Home() {
             <nav>
                 <a href="/products">Produtos</a>
                 <a href="/products?category=vestuario">Categorias</a>
+
+                <button type="button" onClick={productsOpen} >Ver Produtos</button>
             </nav>
         </div>
     )
